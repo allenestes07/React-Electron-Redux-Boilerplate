9f3e3af5fa9702075316fe196613ad17d66398f5
Allen <aestes07@gmail.com>
2019-02-16 13:51:57 -0600

    Add wiring for Sagas to the the store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import reducers from "./../reducers";
+// import rootSaga from './sagas';
+
+import createSagaMiddleware from "redux-saga";
 
 const reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(),
+    applyMiddleware(sagaMiddleware),
     reduxDevTools
   )
 );
+
+// sagaMiddleware.run(rootSaga);
 
 export default store;
